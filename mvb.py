54666cd261bd76778e712072e05d6813de5303dc
maccubgans <66221915+maccubgans@users.noreply.github.com>
2023-03-18 07:33:34 +0700

    Update mvb.py

--- mvb.py
+++ mvb.py
@@ -585,7 +585,7 @@ class Crack:
 							
 	###----------[ METODE API ]---------- ###
 	def metode_api(self,email,pwx):
-		prog.update(des,description=f" {H2}• {P2}[{H2}Mvb🌻P2}] {P2}[{P2}{str(self.loop)}{P2}/{P2}{len(tampung)}{P2}]{P2} [OK : {H2}{len(self.ok)}{P2} CP : {K2}{len(self.cp)}{P2}] [")
+		prog.update(des,description=f" {H2}• {P2}[{H2}Mvb🌻{P2}] {P2}[{P2}{str(self.loop)}{P2}/{P2}{len(tampung)}{P2}]{P2} [OK : {H2}{len(self.ok)}{P2} CP : {K2}{len(self.cp)}{P2}] [")
 		prog.advance(des)
 		try:
 			for pw in pwx:
